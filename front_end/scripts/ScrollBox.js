b278c5e78caa86e31cb387e310a6a5344da945c7
Pete <Peter Lowe>
2017-09-21 11:32:44 -0700

    Add scroll event listner and fade out 'fade-out' at bottom

--- front_end/scripts/ScrollBox.js
+++ front_end/scripts/ScrollBox.js
@@ -18,14 +18,24 @@ var fadeOut = {
 	height:'65%',
 	left:'0%',
 	bottom:'0%',
+	transition:'opacity 0.5s ease-in-out',
 	backgroundImage:"url(/images/fade-out.png)",
 	backgroundSize:'cover'
 }
 
 class ScrollBox extends React.Component {
+	handleScroll(e){
+		var box = e.target
+		var fadeOut = box.getElementsByClassName('fade-out')[0]
+		if (box.scrollTop === (box.scrollHeight - box.offsetHeight)){
+			fadeOut.style.opacity = '0';
+		}else{
+			fadeOut.style.opacity = '1';
+		}
+	}
 	render() {
 		return(
-			<div className='scroll-box' style={scrollBox}>
+			<div className='scroll-box' style={scrollBox} onScroll={this.handleScroll}>
 				<div className='fade-out' style={fadeOut}></div>
 				{this.props.text.split(/\\n/).map(function(text, index){
 					return <p key={index}>{text}</p>
